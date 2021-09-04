deaea01779f0c606e1ed5000c6e9ccf4b6e55768
Masato Sugiyama <public@smasato.net>
2021-09-04 17:48:56 +0900

    update script

--- scripts/symlink.zsh
+++ scripts/symlink.zsh
@@ -1,5 +1,8 @@
 #!/usr/local/bin/zsh
 
+zparseopts -E -D -- \
+    -skhd=o_skhd
+
 # coreutils required
 if [ ! -d /usr/local/opt/coreutils ]; then
     echo "coreutils required run: brew install coreutils"
@@ -7,17 +10,18 @@ if [ ! -d /usr/local/opt/coreutils ]; then
     exit 1
 fi
 
-from=("./zsh/.zshrc" "./zsh/.p10k.zsh" "./tmux/.tmux.conf" "./git/.gitconfig" "./git/.gitignore_global" "./skhd/.skhdrc")
-to=(".zshrc" '.p10k.zsh' '.tmux.conf' '.gitconfig' '.gitignore_global' '.skhdrc')
+from=("./zsh/.zshrc" "./zsh/.p10k.zsh" "./tmux/.tmux.conf" "./git/.gitconfig" "./git/.gitignore_global")
+to=(".zshrc" '.p10k.zsh' '.tmux.conf' '.gitconfig' '.gitignore_global')
 
 if [ "$1" = "link" ]; then
-    from=("./zsh/.zshrc" "./zsh/.p10k.zsh" "./tmux/.tmux.conf" "./git/.gitconfig" "./git/.gitignore_global" "./skhd/.skhdrc")
-    to=(".zshrc" '.p10k.zsh' '.tmux.conf' '.gitconfig' '.gitignore_global' '.skhdrc')
-
     if [ $#from -eq $#to ]; then
         for i in $(seq 1 $#to); do
             (ln -s $(readlink -f ${from[i]}) $HOME/${to[i]}) && echo linked $HOME/${to[i]}
         done
+    fi
+
+    if [[ "$o_skhd" ]]; then
+        ln -s $(readlink -f ./skhd/.skhdrc) ~/.skhdrc && echo "linked $HOME/.skhdrc"
     fi
 
     exit 0
@@ -27,6 +31,10 @@ if [ "$1" = "unlink" ]; then
     for i in $(seq 1 $#to); do
         unlink $HOME/${to[i]} && echo unlinked $HOME/${to[i]}
     done
+
+    if [[ "$o_skhd" ]]; then
+        unlink ~/.skhdrc && echo "unlinked $HOME/.skhdrc"
+    fi
 
     exit 0
 fi
