3b50e9054f8b7ccc8274dc57f2bb21b9a78afd04
Masato Sugiyama <public@smasato.net>
2021-09-04 17:10:26 +0900

    use loop

--- scripts/symlink.zsh
+++ scripts/symlink.zsh
@@ -7,32 +7,26 @@ if [ ! -d /usr/local/opt/coreutils ]; then
     exit 1
 fi
 
+from=("./zsh/.zshrc" "./zsh/.p10k.zsh" "./tmux/.tmux.conf" "./git/.gitconfig" "./git/.gitignore_global" "./skhd/.skhdrc")
+to=(".zshrc" '.p10k.zsh' '.tmux.conf' '.gitconfig' '.gitignore_global' '.skhdrc')
+
 if [ "$1" = "link" ]; then
-    # zsh files
-    ln -s $(readlink -f ./zsh/.zshrc) ~/.zshrc && echo "linked ./zsh/.zshrc"
-    ln -s $(readlink -f ./zsh/.p10k.zsh) ~/.p10k.zsh && echo "linked ./zsh/.p10k.zsh"
+    from=("./zsh/.zshrc" "./zsh/.p10k.zsh" "./tmux/.tmux.conf" "./git/.gitconfig" "./git/.gitignore_global" "./skhd/.skhdrc")
+    to=(".zshrc" '.p10k.zsh' '.tmux.conf' '.gitconfig' '.gitignore_global' '.skhdrc')
 
-    # tmux files
-    ln -s $(readlink -f ./tmux/.tmux.conf) ~/.tmux.conf && echo "linked ./tmux/.tmux.conf"
-
-    # git files
-    ln -s $(readlink -f ./git/.gitconfig) ~/.gitconfig && echo "linked ./git/.gitconfig"
-    ln -s $(readlink -f ./git/.gitignore_global) ~/.gitignore_global && echo "linked ./git/.gitignore_global"
+    if [ $#from -eq $#to ]; then
+        for i in $(seq 1 $#to); do
+            (ln -s $(readlink -f ${from[i]}) $HOME/${to[i]}) && echo linked $HOME/${to[i]}
+        done
+    fi
 
     exit 0
 fi
 
 if [ "$1" = "unlink" ]; then
-    # zsh files
-    unlink ~/.zshrc && echo "unlinked ~/.zshrc"
-    unlink ~/.p10k.zsh && echo "unlinked ~/.p10k.zsh"
-
-    # tmux files
-    unlink ~/.tmux.conf && echo "unlinked ~/.tmux.conf"
-
-    #  git files
-    unlink ~/.gitconfig && echo "unlinked ~/.gitconfig"
-    unlink ~/.gitignore_global && echo "unlinked ~/.gitignore_global"
+    for i in $(seq 1 $#to); do
+        unlink $HOME/${to[i]} && echo unlinked $HOME/${to[i]}
+    done
 
     exit 0
 fi
