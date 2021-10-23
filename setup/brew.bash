f5af67e9b2d6ce5d63a6d20d83608913ea02b4d1
Masato Sugiyama <public@smasato.net>
2021-10-23 13:27:54 +0900

    wip

---
+++ setup/brew.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+brew update
+brew upgrade
+
+brew install coreutils
+brew install curl gawk
+brew install asdf
+brew install brew-cask-completion
+brew install zsh zsh-completions bash fish
+brew install fzf peco
+brew install ghq git git-delta tig
+brew install m-cli rcm
+brew install tmux reattach-to-user-namespace
+brew install --cask visual-studio-code keka karabiner-elements jetbrains-toolbox
