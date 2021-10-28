d63c322bb15ef15b48ed7047b58a0f906d6aa301
Masato Sugiyama <public@smasato.net>
2021-10-29 01:03:54 +0900

    Disable Badge on System Preferences

--- setup/defaults.bash
+++ setup/defaults.bash
@@ -11,15 +11,13 @@ defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false" && killall Finder
 defaults write NSGlobalDomain "NSDocumentSaveNewDocumentsToCloud" -bool "false" 
 
-# systempreferences
-defaults write com.apple.systempreferences AttentionPrefBundleIDs -dict com.apple.preferences.softwareupdate 0
-
 # keyboard
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write -g ApplePressAndHoldEnabled -bool false
 
 # dock
+defaults write com.apple.systempreferences AttentionPrefBundleIDs 0
 defaults write com.apple.dock "orientation" -string "right"
 defaults write com.apple.dock "tilesize" -int "36"
 defaults write com.apple.dock "autohide" -bool "true"
